f8d86a5e6dc77c24e0fe84cb852dd4e15c47a001
Shachar Ron Zohar <88171079+shacharRonZohar@users.noreply.github.com>
2023-02-13 14:39:32 +0200

    start of composable \ mixin refactor

--- src/cmps/backoffice/job/JobMenu.vue
+++ src/cmps/backoffice/job/JobMenu.vue
@@ -4,63 +4,67 @@
       <span class="material-icons"> more_horiz </span>
     </button>
 
-    <div
-      class="menu-modal"
-      :ref="job._id"
-      @contextmenu.stop.prevent=""
+    <div class="menu-modal" :ref="job._id" @contextmenu.stop.prevent=""
       :style="{ top: modalTop, insetInlineStart: modalInlineStart }"
-      :class="{ open: isOpen && !isMobile, top: isBottom }"
-    >
+      :class="{ open: isOpen && !isMobile, top: isBottom }">
       <template v-if="!job.archivedAt">
-        <div @click="onCopyUrl">{{getTrans('copy-invitation-url')}}</div>
-        <div @click="onGoToPage({ name: 'JobEdit', params: { jobId: job._id } })">{{getTrans('edit-job')}}</div>
-        <div @click="onOpenPreview">{{getTrans('preview-job')}}</div>
-        <div @click="onCloneJob">{{getTrans('clone-job')}}</div>
-        <div @click="onShare">{{getTrans('share-job')}}</div>
+        <div @click="onCopyUrl">{{ getTrans('copy-invitation-url') }}</div>
+        <div @click="onGoToPage({ name: 'JobEdit', params: { jobId: job._id } })">{{ getTrans('edit-job') }}</div>
+        <div @click="onOpenPreview">{{ getTrans('preview-job') }}</div>
+        <div @click="onCloneJob">{{ getTrans('clone-job') }}</div>
+        <div @click="onShare">{{ getTrans('share-job') }}</div>
       </template>
-      <div @click="onToggleArchive">{{job.archivedAt ? getTrans('restore') : getTrans('archive-job')}}</div>
-      <div v-if="job.archivedAt" @click="onRemoveJob">{{getTrans('delete-permanently')}}</div>
+      <div @click="onToggleArchive">{{ job.archivedAt ? getTrans('restore') : getTrans('archive-job') }}</div>
+      <div v-if="job.archivedAt" @click="onRemoveJob">{{ getTrans('delete-permanently') }}</div>
     </div>
 
-    <mobile-modal
-      v-if="isOpen && isMobile"
-      cmpName="job-actions"
-      @on-close="toggleModal"
-      :job="job"
-      @on-clone-job="onCloneJob"
-      @on-open-preview="onOpenPreview"
-      @on-share="onShare"
-      @on-toggle-archive="onToggleArchive"
-      @on-copy-url="onCopyUrl"
-      @on-go-to-page="onGoToPage($event)"
-    />
+    <mobile-modal v-if="isOpen && isMobile" cmpName="job-actions" @on-close="toggleModal" :job="job"
+      @on-clone-job="onCloneJob" @on-open-preview="onOpenPreview" @on-share="onShare"
+      @on-toggle-archive="onToggleArchive" @on-copy-url="onCopyUrl" @on-go-to-page="onGoToPage($event)" />
   </section>
 </template>
 
 <script>
+import { ref, computed } from 'vue'
+// Services
 import { msgService } from '@/services/msgService'
-
-import ModalMixin from '@/mixins/ModalMixin.js'
-import cloneDeep from 'lodash.clonedeep'
-
+// Composables
+import { useModal } from '@/composables/useModal'
+// cmps
 import MobileModal from '@/cmps/common/modals/MobileModal.vue'
+// lib
+import cloneDeep from 'lodash.clonedeep'
+// Misc
 import config from '@/config'
 
 export default {
   props: ['job', 'mousePos'],
 
-  mixins: [ModalMixin],
+  // mixins: [ModalMixin],
+  setup(props) {
+    const modalHeight = ref(props.job.archivedAt ? 100 : 300)
+    const modalWidth = ref(200)
 
-  data() {
+    const isOpen = computed(() => {
+      return this.modal.type === 'job-menu' && this.modal.data.modalId === this.job._id
+    })
+
+    const { setModalPosition } = useModal({ isOpen, })
+
     return {
-      modalHeight: this.job.archivedAt ? 100 : 300,
-      modalWidth: 200,
+      // data
+      modalHeight,
+      modalWidth,
+      // methods
+      setModalPosition,
+      // computed
+      isOpen
     }
   },
 
   computed: {
     isMobile() {
-      return this.$store.getters['app/isMobile']
+      return this.$store.getters.app['app/isMobile']
     },
 
     invitationUrl() {
@@ -79,10 +83,9 @@ export default {
     modal() {
       return this.$store.getters['app/modal']
     },
-
-    isOpen() {
-      return this.modal.type === 'job-menu' && this.modal.data.modalId === this.job._id
-    },
+    // isOpen() {
+    //   return this.modal.type === 'job-menu' && this.modal.data.modalId === this.job._id
+    // },
   },
 
   methods: {
--- src/composables/useModal.js
+++ src/composables/useModal.js
@@ -1,6 +1,44 @@
 import { ref, computed, onMounted } from 'vue'
+import {useStore } from 'vuex'
 
-export default function useModal() {
+const props = {
+  isOpen:
+}
+
+// data() {
+//   return {
+//     modalHeight: this.job.archivedAt ? 100 : 300,
+//     modalWidth: 200,
+//   }
+// },
+
+// computed: {
+//   isMobile() {
+//     return this.$store.getters['app/isMobile']
+//   },
+
+//   invitationUrl() {
+//     return `${config.baseUrl}interview/${this.job._id}`
+//   },
+
+//   companyName() {
+//     const name = this.job.company?.name
+//     return name?.charAt(0).toUpperCase() + name?.slice(1)
+//   },
+
+//   jobTitle() {
+//     return this.job.info.title
+//   },
+
+//   modal() {
+//     return this.$store.getters['app/modal']
+//   },
+
+//   isOpen() {
+//     return this.modal.type === 'job-menu' && this.modal.data.modalId === this.job._id
+//   },
+export default function useModal({isOpen,mousePos,}) { 
+  const store = useStore()
  const isBottom = ref(false)
  const modalWidth = ref(0)
  const modalTop = ref(0)
@@ -8,13 +46,13 @@ export default function useModal() {
  const modalPos = ref(null)
 
  const isEnglish = computed(() => {
-  return this.$store.getters['app/lang'] === 'en'
+  return store.getters['app/lang'] === 'en'
  })
 
- onMounted(() => {
+ const setModalPosition=() => {
   if (this.isOpen) return
   const bodyBounding = document.body.getBoundingClientRect()
-  modalPos.value = this.mousePos ? this.mousePos : this.$refs['modal-wrapper'].getBoundingClientRect()
+  modalPos.value = this.mousePos ? this.mousePos : this.$refs['modal-wrapper'].getBoundingClientRect() // not sure how to handle the $refs in the best way yetr
 
   if (this.mousePos) {
    isBottom.value = this.mousePos.y + this.modalHeight > bodyBounding.height
@@ -32,7 +70,7 @@ export default function useModal() {
     modalPos.value.right - modalWidth.value < 0 ? '10px' : modalPos.value.right - modalWidth.value + 'px' :
     bodyBounding.width - modalPos.value.left - modalWidth.value + 'px'
   }
- })
+ }
 
  return {
   isBottom,
@@ -40,6 +78,7 @@ export default function useModal() {
   modalTop,
   modalInlineStart,
   modalPos,
-  isEnglish
+  isEnglish,
+  setModalPosition
  }
 }
