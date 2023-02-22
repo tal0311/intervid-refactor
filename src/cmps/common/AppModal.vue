63979dccb2983b99b7bedebdf930c15f9113e277
Elhanan Avihail <etsAvihail@gmail.com>
2023-02-22 18:54:05 +0200

    AppModal is comeback

--- src/cmps/common/AppModal.vue
+++ src/cmps/common/AppModal.vue
@@ -6,8 +6,8 @@
 </template>
 
 <script>
-import AlertBar from '@/cmps/common/AlertBar'
-import RegularModal from '@/cmps/common/RegularModal'
+import AlertBar from '@/cmps/common/AlertBar.vue'
+import RegularModal from '@/cmps/common/RegularModal.vue'
 
 export default {
   components: {
