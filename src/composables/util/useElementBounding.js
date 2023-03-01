9556b85e1a2e415165ce59aced6403d9403b01fc
Shachar Ron Zohar <88171079+shacharRonZohar@users.noreply.github.com>
2023-03-01 12:40:49 +0200

    updated useElementBounding

--- src/composables/util/useElementBounding.js
+++ src/composables/util/useElementBounding.js
@@ -1,20 +1,22 @@
 import {ref, onMounted, onUnmounted} from 'vue'
 // this composable receives a ref and returns the bounding rect of the element, which updates on scroll
-export function useElementBounding(scrollContainerSelector, elementRef) {
+export function useElementBounding(elementRef) {
   const bounding = ref({})
-
   const updateBounding = () => {
-    console.log('updateBounding')
     if (elementRef.value) {
       bounding.value = elementRef.value.getBoundingClientRect()
     }
-    console.log(bounding.value)
   }
 
   onMounted(() => {
     updateBounding()
+    const observer = new IntersectionObserver(updateBounding, {
+      root: elementRef.value,
+      rootMargin: '0px',
+      threshold: 0.1,
+    })
     // TODO: This is a hack, we need to find a better way to do this
-    document.querySelector(scrollContainerSelector).addEventListener('scroll', updateBounding)
+    // document.querySelector(scrollContainerSelector).addEventListener('scroll', updateBounding)
   })
 
   onUnmounted(() => {
