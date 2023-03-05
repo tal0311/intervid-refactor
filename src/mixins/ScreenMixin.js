cd7e47406a79efe31af6f6d604379ea456c04360
Shachar Ron Zohar <88171079+shacharRonZohar@users.noreply.github.com>
2023-03-05 14:42:25 +0200

    added a temp toggle to loggerService and comments to screenMixin

--- src/mixins/ScreenMixin.js
+++ src/mixins/ScreenMixin.js
@@ -4,22 +4,30 @@ import {screenService} from '@/services/screenService.js'
 
 export default {
   _screenRecorder: null,
-
+  // cmps:
+  // 1 = InterviewIndex
+  // 2 = MediaCheck
   data() {
     return {
+      // internal, one use outside for checking if it exists, 2
       screenStream: null,
-      screenChunks: [],
-      isRecordingScreen: false,
+      // internal + external, 1 + 2
       screenErrors: [],
+      // internal
+      _screenChunks: [],
+      // internal
+      _isRecordingScreen: false,
     }
   },
 
   methods: {
+    // external, 1 + 2
     async initScreenMixin() {
       loggerService.info('[ScreenMixin] [initScreenMixin]')
       try {
         this.screenStream = await screenService.getScreenStream()
-        const elScreenVideo = this.getElScreenVideo()
+        console.log('🚀 ~ file: ScreenMixin.js:29 ~ initScreenMixin ~ this.screenStream:', this.screenStream)
+        const elScreenVideo = this._getElScreenVideo()
         if (elScreenVideo) {
           elScreenVideo.srcObject = this.screenStream
           elScreenVideo.play()
@@ -31,37 +39,33 @@ export default {
         this.removeScreenError(screenErrorMap.DENIED_SCREEN_ACCESS)
         const browserName = this.$store.getters['app/browser'].name
         if (browserName === 'safari') {
-          this.addScreenError(screenErrorMap.SAFARI_SCREEN_SHARE)
+          this._addScreenError(screenErrorMap.SAFARI_SCREEN_SHARE)
           loggerService.info('[ScreenMixin] [initScreenMixin] Safari screen share alert added')
         }
       } catch (err) {
         loggerService.error('[ScreenMixin] [initScreenMixin] Denied screen access', {err})
-        this.addScreenError(screenErrorMap.DENIED_SCREEN_ACCESS)
+        this._addScreenError(screenErrorMap.DENIED_SCREEN_ACCESS)
       }
 
-      this.addStreamStopListener().then((ev) => {
-        loggerService.error('[ScreenMixin] [initScreenMixin] Candidate denied screen access - event: ', {ev})
-        this.addScreenError(screenErrorMap.DENIED_SCREEN_ACCESS)
-      })
+      // this._addStreamStopListener().then((ev) => {
+      //   loggerService.error('[ScreenMixin] [initScreenMixin] Candidate denied screen access - event: ', {ev})
+      //   this._addScreenError(screenErrorMap.DENIED_SCREEN_ACCESS)
+      // })
     },
-
-    getElScreenVideo() {
-      return this.$refs.vidRecorder?.$refs.screenVideo
-    },
-
+    // external, 1
     startScreenRecording() {
       this.isRecordingScreen = true
       this._screenRecorder.start(1000)
       loggerService.info('[ScreenMixin] [startScreenRecording]')
     },
-
+    // external, 1
     stopScreenRecorder() {
       if (this._screenRecorder && this._screenRecorder.state !== 'inactive') {
         this._screenRecorder.stop()
         loggerService.info('[ScreenMixin] [stopScreenRecorder]')
       }
     },
-
+    // external, 1
     stopScreenRecording(interviewId, questId) {
       return new Promise((res) => {
         const createScreenBlob = () => {
@@ -90,14 +94,26 @@ export default {
         loggerService.info('[ScreenMixin] [stopScreenRecording]', {interviewId}, {questId})
       })
     },
-
+    // external, 1
     disposeScreenStream() {
       if (!this.screenStream) return
       this.screenStream = screenService.disposeScreenStream()
       loggerService.info('[ScreenMixin] [disposeScreenStream]')
     },
-
-    addStreamStopListener() {
+    // ineternal + external, 1 + 2
+    removeScreenError(errorToRemove) {
+      if (this.screenErrors.some((err) => err.type === errorToRemove.type)) {
+        this.screenErrors = this.screenErrors.filter((error) => error.type !== errorToRemove.type)
+        loggerService.info('[ScreenMixin] [removeScreenError] Screen error removed', {errorToRemove})
+      }
+    },
+    // internal
+    _getElScreenVideo() {
+      // why the actual fuck
+      return this.$refs.vidRecorder?.$refs.screenVideo
+    },
+    // internal
+    _addStreamStopListener() {
       // Adds stop stream events -> In order to get the event everytime the stream stops.
       loggerService.info('[ScreenMixin] [addStreamStopListener]')
       return new Promise((resolve, reject) => {
@@ -125,20 +141,13 @@ export default {
         }
       })
     },
-
-    addScreenError(err) {
+    // internal
+    _addScreenError(err) {
       if (this.screenErrors.includes(err)) return
       this.screenErrors.push(err)
       loggerService.info('[ScreenMixin] [addScreenError] Screen error added', {
         err,
       })
-    },
-
-    removeScreenError(errorToRemove) {
-      if (this.screenErrors.some((err) => err.type === errorToRemove.type)) {
-        this.screenErrors = this.screenErrors.filter((error) => error.type !== errorToRemove.type)
-        loggerService.info('[ScreenMixin] [removeScreenError] Screen error removed', {errorToRemove})
-      }
     },
   },
 }
