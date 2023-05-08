862d839cfd9dec0e08b49e480aed2f116a01e22f
Shachar Ron Zohar <88171079+shacharRonZohar@users.noreply.github.com>
2023-05-08 10:59:36 +0300

    bug(user-modal-scroll): activated old directive, small tweaks to it

--- src/directivs.js
+++ src/directivs.js
@@ -1,6 +1,9 @@
-const clickOutside = {
-  mounted(el, {value: cb}) {
+// @vicky - other directive may work if ypu have problems
+export const clickOutside = {
+  mounted(el, {value: cb, modifiers}) {
     el.clickOutside = (ev) => {
+      if (modifiers.prevent) ev.preventDefault()
+      if (modifiers.stop) ev.stopPropagation()
       if (!el.contains(ev.target)) {
         // itemService.setModalType("");
         cb()
@@ -18,30 +21,26 @@ const clickOutside = {
   },
 }
 
-// @vicky - other directive may work if ypu have problems
-// export const clickOutsideDirective = {
-//   mounted(el, { value: cb }) {
-//     el.clickOutside = ({ clientX, clientY }) => {
-//       const { left, top, width, height } = el.getBoundingClientRect()
-//       if (
-//         !(clientX > left &&
-//           clientX < left + width &&
-//           clientY > top &&
-//           clientY < top + height)
-//       ) {
-//         cb()
-//         console.log('outside')
-//       } else {
-//         console.log('inside')
-//       }
-//     }
-//     setTimeout(() => {
-//       document.addEventListener('click', el.clickOutside)
-//     }, 0)
-//   },
-//   unmounted(el) {
-//     document.removeEventListener('click', el.clickOutside)
-//   },
-// }
-
-export {clickOutside}
+// @shachar - had problems with other directive lol
+export const clickOutsideCalc = {
+  mounted(el, {value: cb, modifiers}) {
+    el.clickOutside = (ev) => {
+      if (modifiers.prevent) ev.preventDefault()
+      if (modifiers.stop) ev.stopPropagation()
+      const {clientX, clientY} = ev
+      const {left, top, width, height} = el.getBoundingClientRect()
+      if (!(clientX > left && clientX < left + width && clientY > top && clientY < top + height)) {
+        cb()
+        // console.log('outside')
+      } else {
+        // console.log('inside')
+      }
+    }
+    queueMicrotask(() => {
+      document.addEventListener('click', el.clickOutside)
+    })
+  },
+  unmounted(el) {
+    document.removeEventListener('click', el.clickOutside)
+  },
+}
