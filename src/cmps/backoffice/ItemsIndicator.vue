aefdbf270dbae916a0a950e343672507f3bf5b9d
tal0311 <tal.amit0311@gmail.com>
2023-05-09 15:28:20 +0300

    fix typo on actions model sayes iteames converted to items

--- src/cmps/backoffice/ItemsIndicator.vue
+++ src/cmps/backoffice/ItemsIndicator.vue
@@ -1,14 +1,10 @@
 <template>
   <section class="items-indicator flex align-items-center">
-    <span
-      v-for="item in selectedItems.slice(0, 10)"
-      :key="item.id"
-      class="material-icons"
-      :style="{color: getColorByStatus(item)}"
-    >
+    <span v-for="item in selectedItems.slice(0, 10)" :key="item.id" class="material-icons"
+      :style="{color: getColorByStatus(item)}">
       circle
     </span>
-    <span v-if="selectedItems.length > 10" class="selected-iteams-count"> +{{ selectedItems.length - 10 }}</span>
+    <span v-if="selectedItems.length > 10" class="selected-items-count"> +{{ selectedItems.length - 10 }}</span>
   </section>
 </template>
 
